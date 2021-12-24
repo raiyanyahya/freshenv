e3e927cc3e4757a6939b1cd91071e42089c3cac6
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2021-12-24 16:02:13 +0100

    release 0.0.4

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 from os.path import abspath, dirname, join
 from setuptools import find_packages, setup
-import logging, os, sys
 
+version_file = ".version"
 this_dir = abspath(dirname(__file__))
 with open(join(this_dir, "README.md"), encoding="utf-8") as file:
     long_description = file.read()
 
 
 def get_version():
-    logging.basicConfig(level=logging.INFO, format="%(name)s: %(message)s")
-    logger = logging.getLogger("setup")
-    if os.path.isfile(".version"):
-        with open(".version") as version_file:
+    if os.path.isfile(version_file):
+        with open(version_file) as version_file:
             version = version_file.read().strip()
-            logger.info("Got {} from .version".format(version))
             return version
     elif (
         "build" in sys.argv
@@ -22,17 +19,10 @@ def get_version():
         or "bdist_wheel" in sys.argv
     ):
         version = os.environ.get("VERSION", "0.0")  # Avoid PEP 440 warning
-        logger.info("Got version {} from environment".format(version))
         if "-SNAPSHOT" in version:
             version = version.replace("-SNAPSHOT", ".0")
-            logger.info(
-                "Replaced '-SNAPSHOT' in version with '.0', resulting in version {}".format(
-                    version
-                )
-            )
-        with open(".version", "w+") as version_file:
+        with open(version_file, "w+") as version_file:
             version_file.write(version)
-            logger.info("Wrote {} to .version".format(version))
         return version
 
 
@@ -46,10 +36,9 @@ setup(
     url="https://github.com/raiyanyahya/freshenv",
     author="Raiyan Yahya",
     author_email="raiyanyahyadeveloper@gmail.com",
-    keywords="cli",
+    keywords=["cli","developer-tools","productivity"],
     packages=find_packages(),
-    install_requires=["click", "docker", "rich", "dockerpty", "pytest"],
-    extras_require={"test": ["coverage", "pytest", "pytest-cov"]},
+    install_requires=["click==7.1.2", "docker==4.3.1", "rich==10.16.1", "dockerpty==0.4.1"],
     entry_points={"console_scripts": ["freshenv=freshenv.cli:cli"]},
     tests_require=["mock >= 2.0.0"],
 )
