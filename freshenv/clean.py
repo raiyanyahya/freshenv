712d20bab0aefdcb9b5cc1ad44ccd65502449f39
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2022-01-10 23:18:10 +0100

    Update clean.py

--- freshenv/clean.py
+++ freshenv/clean.py
@@ -13,13 +13,11 @@ def clean(force: bool) -> None:
         client = APIClient(base_url="unix://var/run/docker.sock")
         freshenv_containers = client.containers(all=True, filters={"name": "freshenv_*"})
         for container in freshenv_containers:
-            print(f":boom: {container['Names'][0]} environment removed.")
             client.remove_container(container=container['Id'], force=force)
         images_list = client.images(filters={"label": "maintainer=Raiyan Yahya <raiyanyahyadeveloper@gmail.com>"})
         for image in images_list:
-            print(f":boom: {image['RepoTags'][0]} image removed.")
             client.remove_image(image=image['Id'], force=force)
-        print(f":boom: freshenv flavours and environments removed.")
+        print(":boom: freshenv flavours and environments removed.")
     except errors.DockerException:
         print(":cross_mark_button: Docker not installed or running. ")
     except errors.APIError as e:
