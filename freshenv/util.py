f190ed078a85e470b1fc641a38bf95d08422ca4e
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2022-05-20 15:28:25 +0000

    codacy fixes

--- freshenv/util.py
+++ freshenv/util.py
@@ -2,8 +2,8 @@ from click import Option, BadParameter
 from ast import literal_eval
 
 class PythonLiteralOption(Option):
+    
     """A utility class to convert a string to a python literal."""
-
 
     def type_cast_value(self, ctx, value):
         try:
